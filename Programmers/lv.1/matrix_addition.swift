34890912059d5ac010a74767dc3d54aac695276d
eelijus <yoagi110@gmail.com>
2022-07-02 11:20:25 +0900

    matrix addition

--- Programmers/lv.1/matrix_addition.swift
+++ Programmers/lv.1/matrix_addition.swift
@@ -7,6 +7,22 @@ func solution(_ arr1:[[Int]], _ arr2:[[Int]]) -> [[Int]] {
     for j in 0..<arr1[i].count {
       result[i].append(arr1[i][j] + arr2[i][j])
     }
-  }
+  }~
   return result
 }
+
+/*
+
+//따로 빈 이차원배열에 빈 일차원배열을 하나하나 붙일 필요 없는 풀이
+
+func solution(_ arr1:[[Int]], _ arr2:[[Int]]) -> [[Int]] {
+    var answer: [[Int]] = arr1
+    for i in 0..<arr1.count {
+        for j in 0..<arr1[i].count {
+            answer[i][j] += arr2[i][j]
+        }
+    }
+    return answer
+}
+
+*/
